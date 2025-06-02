07d28f4f10958d71c140e575414f0cea118eb928
Lakshay Lalia <145219264+lakshaylalia@users.noreply.github.com>
2025-06-03 01:39:21 +0530

    Remove serverless API configuration and update MongoDB connection URI

--- api/index.js
+++
@@ -1,4 +0,0 @@
-const serverless = require("serverless-http");
-const app = require("../server.js"); // adjust if your server.js is renamed or moved
-
-module.exports = serverless(app);
--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI_WEB)
 .then(() => console.log('Connected to MongoDB...')) 
 .catch((err) => console.log(err));
 
